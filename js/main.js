3a43d77d87781ae610150991c39f3a26f1c907ae
Yevenii <vekaev4@gmail.com>
2019-11-21 14:15:06 +0200

    few changes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
-var lists = document.querySelectorAll("#lists .list"),
-    curList = 0;
+// curList = 0;
 // listInteral = setInterval(nextList, 2000);
-console.log(lists);
-lists.onclick = function(event) {
-    let target = event.target;
-    if (target.className != "active") return;
-    highlight(target);
-}
+// console.log(lists);
+// lists.onclick = function(event) {
+//     let target = event.target;
+//     if (target.className != "active") return;
+//     target.classList.add();
+// }
 
-function highlight(active) {
-    if (selectedClass) {
+// function highlight(active) {
+//     if (selectedClass) {
 
-    }
-}
+//     }
+// }
+
+
+
+// lists.addEventListener("click", function() {
+//     lists[0].classList.add("active");
+// });
+window.addEventListener('keydown', function(e) {
+    var lists = document.querySelector(`.list[keyCode="${e.keyCode}"]`);
+
+    lists.forEach(function(item) {
+
+        item.addEventListener("click", function() {
+            let target = event.item;
+
+            lists[e].classList.add("active");
+            console.log(item);
+            console.log(target);
+        })
+    });
+});
